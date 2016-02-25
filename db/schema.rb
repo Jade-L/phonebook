39082773c2219e727dbc97d0a57e15ab5728b6d0
Jade-L <lemaissi.jade@gmail.com>
2016-02-25 10:05:05 +0100

    add information to user: phone address gender subcribe city citycode

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224150251) do
+ActiveRecord::Schema.define(version: 20160225090252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20160224150251) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
+    t.boolean  "gender"
+    t.boolean  "subscribe"
+    t.string   "address"
+    t.string   "citycode"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
