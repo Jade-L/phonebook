846850ebed5babe122e7691f51a01fb29adb0e95
Jade-L <lemaissi.jade@gmail.com>
2016-02-25 10:58:11 +0100

    clean db useless (brand & product) migrate add new db

--- db/schema.rb
+++ db/schema.rb
@@ -16,22 +16,6 @@ ActiveRecord::Schema.define(version: 20160225090614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "brands", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "brand_id"
-    t.string   "name"
-  end
-
-  add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -57,5 +41,4 @@ ActiveRecord::Schema.define(version: 20160225090614) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "products", "brands"
 end
