022c0e1b1885f092993268b18589f257b676feb6
Jade-L <lemaissi.jade@gmail.com>
2016-02-25 22:22:23 +0100

    add def for newsletter

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,14 @@ class UserMailer < ApplicationMailer
     @url  = 'http://localhost:3000/users/sign_in'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def newsletter(user)
+    @users = User.find_by(subcribe: true)
+    @url  = 'http://localhost:3000/users/sign_in'
+    @users.each do |user|
+      mail(to: user.email, subject: 'Come back !')
+    end
+  end
+
+
 end
